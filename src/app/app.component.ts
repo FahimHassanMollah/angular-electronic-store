c7abd7ba5a0e5732e4419122ba5b3b8438d8c6af
FahimHassanMollah <fahimhassan0197@gmail.com>
2023-12-13 17:10:29 +0600

    Add @ng-bootstrap/ng-bootstrap dependencies

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-
+import { NgbAlertModule } from '@ng-bootstrap/ng-bootstrap';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet,NgbAlertModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
