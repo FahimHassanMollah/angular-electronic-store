1c8dbd762498cb799cf0f233eabc6bdd53c2a5e4
FahimHassanMollah <fahimhassan0197@gmail.com>
2023-12-18 01:25:44 +0600

    add navbar component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,18 +3,20 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { NgbAlertModule } from '@ng-bootstrap/ng-bootstrap';
 import { ToastrService } from 'ngx-toastr';
+import { NavbarComponent } from './components/navbar/navbar.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet,NgbAlertModule],
+  imports: [CommonModule, RouterOutlet,NgbAlertModule,NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'angular-electronic-store';
   constructor(private toastr: ToastrService) {
-    this.toastr.success('Hello world!', 'Toastr fun!', {
-      timeOut: 30000,
-    });
+    // this.toastr.success('Hello world!', 'Toastr fun!', {
+    //   timeOut: 30000,
+    // });
    }
 }
