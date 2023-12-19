8adecf09b7c6b280cbcadbc08116842fa5e63718
FahimHassanMollah <fahimhassan0197@gmail.com>
2023-12-19 16:54:35 +0600

    Add User model and signup form

--- src/app/pages/signup/signup.component.ts
+++ src/app/pages/signup/signup.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { User } from '../../models/user.model';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css'
 })
 export class SignupComponent {
+  public user: User;
+  constructor() {
+    this.user = new User('','','','','');
 
+  }
+  signUpFormSubmitHandler(event : any ) {
+    event.preventDefault();
+  }
 }
