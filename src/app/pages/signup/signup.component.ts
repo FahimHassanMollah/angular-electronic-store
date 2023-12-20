c2cc8bb5bb3d900fe5fee369b35879d0f8a60621
FahimHassanMollah <fahimhassan0197@gmail.com>
2023-12-20 17:13:04 +0600

    Refactor signup component form to use reactive forms

--- src/app/pages/signup/signup.component.ts
+++ src/app/pages/signup/signup.component.ts
@@ -1,21 +1,35 @@
 import { Component } from '@angular/core';
 import { User } from '../../models/user.model';
-import { FormsModule } from '@angular/forms';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule,ReactiveFormsModule],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css'
 })
 export class SignupComponent {
   public user: User;
+  signUpForm! : FormGroup;
+
+
   constructor() {
     this.user = new User('','','','','');
+    this.signUpForm = new FormGroup({
+      email: new FormControl('',[Validators.required,Validators.email]),
+      password: new FormControl('',[Validators.required,Validators.minLength(6)]),
+      name : new FormControl('',[Validators.required,Validators.minLength(3)]),
+      gender : new FormControl('',[Validators.required]),
+      about : new FormControl('',[Validators.required]),
+    });
 
   }
-  signUpFormSubmitHandler(event : any ) {
-    event.preventDefault();
+  signUpFormSubmitHandler() {
+    if (this.signUpForm.valid) {
+
+    }
+    // event.preventDefault();
+    console.log(this.signUpForm);
   }
 }
